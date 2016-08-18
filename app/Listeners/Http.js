f15b11576c5f7d6ccc409c0ac117b79f262e33f9
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-18 17:28:46 -0500

    Updated JSON API error handling

--- app/Listeners/Http.js
+++ app/Listeners/Http.js
@@ -5,38 +5,38 @@ const Ouch = use('youch');
 const Http = exports = module.exports = {};
 
 /**
- * handle errors occured during a Http request.
- *
- * @param  {Object} error
- * @param  {Object} request
- * @param  {Object} response
- */
+* handle errors occured during a Http request.
+*
+* @param  {Object} error
+* @param  {Object} request
+* @param  {Object} response
+*/
 Http.handleError = function * (error, request, response) {
-  /**
-   * DEVELOPMENT REPORTER
-   */
+/**
+ * DEVELOPMENT REPORTER
+ */
   if (Env.get('NODE_ENV') === 'development') {
-    const ouch = new Ouch().pushHandler(
-      new Ouch.handlers.PrettyPageHandler('blue', null, 'sublime')
-    );
-    ouch.handleException(error, request.request, response.response, () => {
-      console.error(error.stack);
-    });
+    return (new Ouch)
+      .pushHandler((new Ouch.handlers.JsonResponseHandler(
+            /* handle errors from ajax and json request only*/false,
+            /* return formatted trace information along with error response*/false,
+            false
+        )))
+      // .pushHandler(new Ouch.handlers.PrettyPageHandler())
+      .handleException(error, request.request, response.response, (output) => {
+        const status = error.status || 500;
 
-    return;
+        response.status(status).send(JSON.parse(output));
+        console.log('Error handled properly');
+      });
   }
 
-  /**
-   * PRODUCTION REPORTER
-   */
-  const status = error.status || 500;
-  console.error(error.stack);
-  yield response.status(status).sendView('errors/index', { error });
+  yield response.jsonApiError(error);
 };
 
 /**
- * listener for Http.start event, emitted after
- * starting http server.
- */
+* listener for Http.start event, emitted after
+* starting http server.
+*/
 Http.onStart = function () {
 };
